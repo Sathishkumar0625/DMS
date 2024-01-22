974cddbe6593c4b32854723da81b53451d715ea3
sathish0625 <sathishkumar@proflaut.com>
2024-01-22 21:29:25 +0530

    updated

---
+++ src/main/java/com/proflaut/dms/entity/ProfMetaDataEntity.java
@@ -0,0 +1,67 @@
+package com.proflaut.dms.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PROF_METADATA")
+public class ProfMetaDataEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	@Column(name="TABLE_NAME")
+	private String tableName;
+	@Column(name="FILE_EXTENSION")
+	private String fileExtension;
+	@Column(name="CREATED BY")
+	private String createdBy;
+	@Column(name="CREATED AT")
+	private String createdAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTableName() {
+		return tableName;
+	}
+
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+
+	public String getFileExtension() {
+		return fileExtension;
+	}
+
+	public void setFileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
