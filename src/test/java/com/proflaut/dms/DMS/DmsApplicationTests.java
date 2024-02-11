459cf6e12a92b19a4ab5b930d2ef8ccf2e167d62
sathish0625 <154213852+sathish0625@users.noreply.github.com>
2024-02-11 15:49:20 +0530

    Update DmsApplicationTests.java

--- src/test/java/com/proflaut/dms/DMS/DmsApplicationTests.java
+++ src/test/java/com/proflaut/dms/DMS/DmsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.proflaut.dms.DMS;
+//package com.proflaut.dms.DMS;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DmsApplicationTests {
+//@SpringBootTest
+//class DmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
-}
+//}
